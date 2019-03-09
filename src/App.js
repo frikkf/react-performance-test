f0b4a68a6b64ef58795b48de7bdd48eca538a744
Frikk <frikk.fossan@acando.no>
2019-03-09 11:11:35 +0100

    adds material ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import Button from '@material-ui/core/Button';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: purple[500] }, // Purple and green play nicely together.
+    secondary: { main: '#11cb5f' }, // This is just green.A700 as hex.
+  },
+  typography: { useNextVariants: true },
+});
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-            Learn React
-          
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+
+          <Button color="primary">Primary</Button>
+      <Button color="secondary">Secondary</Button>
+
         </header>
-      </div>
+        </div>
+      </MuiThemeProvider>
+
+
     );
   }
 }
